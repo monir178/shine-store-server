3770d784238716f3e288978890045967672bdd20
monir178 <monir.mzs17@gmail.com>
2024-05-23 21:20:10 +0600

    vercel solved support

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ let client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         console.log('Connected to MongoDB');
 
         const database = client.db('shineStore');
